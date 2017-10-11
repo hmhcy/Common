a4cb58ca8dee7f22b13fc0ebb32309f1aa681e27
Cynthia <cynthiahmh@gmail.com>
2017-10-11 09:58:33 +0800

    Bug修正

--- JS/loadMore.js
+++ JS/loadMore.js
@@ -1,41 +1,59 @@
+import Vue from 'vue'
+
 module.exports = {
   data () {
     return {
       list: [],
       page: 1,
-      limit: 10,
+      pageSize: 10,
       total: 0,
       lock:false, //一次只能发送一个请求,避免请求溢出
-      scrollable:null, //如需要滚动到底部时自动加载数据,则指定可滚动的ui对象
+      scrollable:"article.main", //如需要滚动到底部时自动加载数据,则指定可滚动的ui对象
       loadMoreTarget:null, //装载列表的ui对象,用于获取底部的距离
       positionState:true, //记录加载状态开关,如为true,则重载页面时自动恢复定位
-      stateObject:{}
+      stateObject:{},
+      pathName:""
     }
   },
   created () {
-    this.initList()
+    console.log("created")
+    this.pathName=this.$route.path
+    this.init()
+  },
+  beforeDestroy(){
+    this.onLeave()
   },
   watch: {
-    page: 'loadData'
+    page(val){
+      if(val > 1)this.loadData();
+    }
   },
   methods: {
+    /**
+     * 初始化事件,可重写
+     */
+    init(){
+      this.initList()
+    },
     /**
      * 初始化列表
      */
     initList () {
-      this.stateObject = JSON.parse(sessionStorage.getItem(this.$route.path));
+      this.stateObject = JSON.parse(sessionStorage.getItem(this.pathName));
       this.page = 1;
-      if(this.stateObject && this.stateObject.page) this.limit *= this.stateObject.page;
+      if(this.stateObject && this.stateObject.page) this.pageSize *= this.stateObject.page;
       this.list = [];
+      this.lock = true;
+      this.$broadcast("empty-list",false);
       this.loadData();
     },
     /**
      * 加载更多
      */
     loadMore () {
-      if(!this.lock && this.total > this.page * this.limit){
+      if(!this.lock && this.total > this.page * this.pageSize){
         this.page++;
-        this.limit = 10;
+        //this.pageSize = 2;
         this.lock = true;
       }
     },
@@ -53,6 +71,7 @@ module.exports = {
       }
       })
       this.lock = false;
+      this.$broadcast("empty-list",this.total < 1);
       if(this.scrollable){
         this.$nextTick(this.scroll);
       }
@@ -66,12 +85,11 @@ module.exports = {
         $( _self.scrollable).scrollTop(_self.stateObject.top)
         sessionStorage.removeItem(this.$route.path)
       }
-      $( _self.scrollable).scroll(function() {
-        var target = $(_self.loadMoreTarget).children(":last-child");
-        var offset = target.position().top;
-        var elementHeight = target.height();
-        console.log(offset)
-        if(offset < elementHeight * 2){
+      $( _self.scrollable).bind('scroll', function() {
+        var target = this.scrollHeight - 150,
+          top = $(this).scrollTop(),
+          elementHeight = $(this).height();
+        if(target < elementHeight + top){
           _self.loadMore();
         }
       });
@@ -88,8 +106,8 @@ module.exports = {
      * 使用vue-router中的deactivate钩子触发离开页面的事件,则页面路径为transition.from.path
      * @param name
      */
-    onLeave (name) {
-      sessionStorage.setItem(name,JSON.stringify({top:$(this.scrollable).scrollTop(),page:this.page}));
+    onLeave () {
+      sessionStorage.setItem(this.pathName,JSON.stringify({top:$(this.scrollable).scrollTop(),page:this.page}));
     }
   }
 }
